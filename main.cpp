0cb93e4f8107e48cf8b4bb21babff80b7edd6155
León G <A00573074@itesm.mx>
2022-10-11 05:49:32 +0000

    New

--- main.cpp
+++ main.cpp
@@ -7,6 +7,63 @@ void espacio() {
     cout << endl;
 }
 
+int distanciaMinima(vector <int> distancia, vector<bool> visitado) {
+
+    int minimo;
+    int indice;
+    minimo = 999999999;
+    indice = 0;
+
+    for (int i = 0; i < distancia.size(); i++) {
+        if (visitado[i] == false && distancia[i] <= minimo) {
+            minimo = distancia[i];
+            indice = i;
+        }
+    }
+
+    return indice;
+}
+
+void dijkstra(vector <vector <int>> matriz, int nodo) {
+
+    vector<int> distancia;
+    vector<bool> visitado;
+
+    for (int i = 0; i < matriz.size(); i++) {
+        distancia.push_back(999999999);
+        visitado.push_back(false);
+    }
+
+    distancia[nodo] = 0;
+
+    for (int i = 0; i < matriz.size(); i++) {
+        int minimo = distanciaMinima(distancia, visitado);
+        visitado[minimo] = true;
+        for (int j = 0; j < matriz.size(); j++) {
+            if (!visitado[j] && matriz[minimo][j] && distancia[minimo] != 999999999 && distancia[minimo] + matriz[minimo][j] < distancia[j]) {
+                distancia[j] = distancia[minimo] + matriz[minimo][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < matriz.size(); i++) {
+        char letraOrigen = 65 + i;
+        char letraDestino = 65 + nodo;
+        if (distancia[i] > 0) {
+            cout << "node " << nodo + 1 << " to node " << i + 1 << " : " << distancia[i] << endl;
+        }
+    }
+}
+
+void edsgerDijkstra(vector <vector <int>> matriz) {
+
+    cout << "Dijkstra :" << endl;
+
+    for (int i = 0; i < matriz.size(); i++) {
+        dijkstra(matriz, i);
+    }
+}
+
 void imprimirMatriz(vector <vector <int>> matriz) {
     for (int i = 0; i < matriz.size(); i++) {
         for (int j = 0; j < matriz[i].size(); j++) {
@@ -17,8 +74,10 @@ void imprimirMatriz(vector <vector <int>> matriz) {
 }
 
 void floydWarshall(vector <vector <int>> matriz) {
+
     cout << "Floyd :" << endl;
     vector <vector <int>> distancia = matriz;
+
     for (int i = 0; i < matriz.size(); i++) {
         for (int j = 0; j < matriz.size(); j++) {
             for (int k = 0; k < matriz.size(); k++) {
@@ -28,15 +87,19 @@ void floydWarshall(vector <vector <int>> matriz) {
             }
         }
     }
+
     imprimirMatriz(distancia);
 }
 
 int main() {
+
     int n;
     int menosUno;
-    menosUno = 999999999;
-    cin >> n;
     vector <vector <int>> matriz;
+    menosUno = 999999999;
+
+    cin >> n;
+    
     for (int i = 0; i < n; i++) {
         vector <int> auxiliar;
         for (int j = 0; j < n; j++) {
@@ -51,6 +114,9 @@ int main() {
         }
         matriz.push_back(auxiliar);
     }
+
+    edsgerDijkstra(matriz);
     floydWarshall(matriz);
+
     return 0;
 }
